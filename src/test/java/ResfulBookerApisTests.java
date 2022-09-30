6017ab059079c89856007905e3f360c1c7496d7f
ohood <msohood@gmail.com>
2022-09-30 09:50:52 +0200

    First Commit

--- src/test/java/ResfulBookerApisTests.java
+++ src/test/java/ResfulBookerApisTests.java
@@ -1,15 +1,10 @@
-package apiTests;
-
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-
-
 public class ResfulBookerApisTests {
     // login as precondition
     String accessToken;
